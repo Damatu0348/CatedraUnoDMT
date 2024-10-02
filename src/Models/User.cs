2bb030ab7e96c0bc67659f55a69ffc8a573ff791
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-10-02 20:26:14 -0300

    feat: models creados

---
+++ src/Models/User.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.src.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [StringLength(12, MinimumLength = 9)]
+        public string Rut { get; set; } = string.Empty;
+        [StringLength(100, MinimumLength = 3)]
+        public string Nombre { get; set; } = string.Empty;
+        [EmailAddress(ErrorMessage = "Correo electrónico inválido.")]
+        public string Correo { get; set; } = string.Empty;
+        [RegularExpression(@"Masculino|Femenino|Otro|Prefiero no decirlo")]
+        public string Genero { get; set; } = string.Empty;
+         [DataType(DataType.Date)]
+        [CustomValidation(typeof(User), "ValidateBirthDate")]
+        public string FechaNacimiento { get; set; } = string.Empty;
+
+        public static ValidationResult? ValidateBirthDate(DateTime birthDate, ValidationContext context)
+        {
+            return birthDate < DateTime.Now
+                ? ValidationResult.Success
+                : new ValidationResult("La fecha de nacimiento debe ser menor a la fecha actual.");
+        }
+    }
+}

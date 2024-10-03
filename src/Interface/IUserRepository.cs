d356008a34f04fe0b100815a9616859b429d5174
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-10-02 23:53:22 -0300

    fi: arreglado interfaces

--- src/Interface/IUserRepository.cs
+++ src/Interface/IUserRepository.cs
@@ -7,9 +7,10 @@ using api.src.Models;
 
 namespace api.src.Interface
 {
-    public class IUserRepository
+    public interface IUserRepository
     {
         Task<List<User>> GetAll();
+        Task<User?> GetById(int id);
         Task<User> Post(User user);
         Task<User?> Put(int id, UpdateUserRequestDto userDto);
         Task<User?> Delete(int id);
